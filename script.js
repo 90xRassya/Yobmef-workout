39fc791474c7de2847eadf6dc22933babf94ec03
90xRassya <90tarat@gmail.com>
2026-08-19 08:55:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ document.getElementById("catatan").value = data;
 }
 
 }
+function tambahTask() {
+
+    let input = document.getElementById("task");
+    let tugas = input.value;
+
+    if (tugas === "") {
+        alert("Tulis tugas dulu!");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        <span onclick="selesaiTask(this)">
+            ☐ ${tugas}
+        </span>
+
+        <button onclick="hapusTask(this)">
+            🗑
+        </button>
+    `;
+
+    document.getElementById("list").appendChild(li);
+
+    input.value = "";
+}
+
+function selesaiTask(element) {
+
+    if (element.innerHTML.startsWith("☐")) {
+        element.innerHTML = element.innerHTML.replace("☐", "☑");
+    } else {
+        element.innerHTML = element.innerHTML.replace("☑", "☐");
+    }
+}
+
+function hapusTask(button) {
+    button.parentElement.remove();
+}
