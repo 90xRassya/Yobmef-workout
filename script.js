56630c40d7f9003802c1b471d9173c44d3009149
90xRassya <90tarat@gmail.com>
2026-08-19 08:11:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const tombol = document.getElementById("tombol");
 tombol.addEventListener("click", function () {
     alert("Halo! Selamat datang di website latihanmu 🎉");
 });
+function simpanCatatan(){
+
+let isi = document.getElementById("catatan").value;
+
+localStorage.setItem("catatanSaya", isi);
+
+document.getElementById("status").innerHTML =
+"✅ Catatan berhasil disimpan";
+}
+
+window.onload = function(){
+
+let data = localStorage.getItem("catatanSaya");
+
+if(data){
+document.getElementById("catatan").value = data;
+}
+
+}
